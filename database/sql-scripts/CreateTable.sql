b079eb01fe2e1e5ea890f8e2c3e6d08217ac08dc
samratk-cuelogic <samrat.kasture@cuelogic.com>
2019-08-01 12:33:58 +0530

    test

---
+++ database/sql-scripts/CreateTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `template` (
+  `title` varchar(25) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1
